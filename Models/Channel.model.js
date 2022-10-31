b38b6e734e176f5ec3796b5380fcdcee6e9a8bc6
kedar_devs <kedard249.kd@gmail.com>
2022-10-31 12:21:30 +0530

    Feat:Create Channel Model

---
+++ Models/Channel.model.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+const ChannelSchema=Schema({
+    channelName:{type:String,reqired:true},
+    channelSubCount:{type:Number,required:true},
+    playlist:{type:[String]},
+    about:{type:String,required:true},
+    socialInsta:{type:String},
+    socialFB:{type:String},
+    socialTwit:{type:String},
+    channelCreator:{type:mongoose.Schema.Types.ObjectId}
+})
