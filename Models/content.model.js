5196e3b04abc1ce5a485d3a331c4528da5cdc5c5
kedar_devs <kedard249.kd@gmail.com>
2022-10-31 12:22:08 +0530

    Feat:Create Content Model

--- Models/content.model.js
+++ Models/content.model.js
@@ -1,2 +1,12 @@
+const { Double } = require('mongodb')
 const mongoose=require('mongoose')
 const Schema=mongoose.Schema
+const ContentSchema=Schema({
+    ContentUrl:{type:String,required:true},
+    ContentCreator:{type:mongoose.Schema.Types.ObjectId,required:true},
+    LikeCount:{type:Number},
+    DislikeCount:{type:Number},
+    length:{type:Double},
+    viewCount:{type:Number},
+        
+})
