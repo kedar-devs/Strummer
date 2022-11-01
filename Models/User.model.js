beedc0a78c604223193b58542151a0c0438bdae8
kedar_devs <kedard249.kd@gmail.com>
2022-11-01 21:58:44 +0530

    Fix:Add accessToken and refreshToken

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,15 +1,17 @@
 const mongoose=require('mongoose')
 const Schema=mongoose.Schema
-
+const Coach=require('./Coach.model')
 const UserDetail=Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true},
-    pasword:{type:String,required:true},
+    name:{type:String,required:true,default:'User123'},
+    email:{type:String,unique:true,required:true},
+    pasword:{type:String,required:true,default:'User@123'},
     Courses:{type:[String]},
     contact:{type:Number,required:true},
     profilePic:{type:String,required:false},
     isCoach:{type:Boolean,required:true},
-    CoachId:{type:mongoose.Schema.Types.ObjectId}
+    CoachId:{type:mongoose.Schema.Types.ObjectId,ref:"Coach"},
+    accessToken:{type:String,required:true,expireAfterSeconds: 864000 },
+    refreshToken:{type:String,required:true}
     
 })
 const UserData=new mongoose.Model('User',UserDetail)
