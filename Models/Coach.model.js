7d4e6328ef7e224d60953cf2d90abaebea6c4e0b
kedar_devs <kedard249.kd@gmail.com>
2022-11-02 10:54:58 +0530

    Delete unnecesary attribute
    Delete channel name and expense as they already exist in channel model

--- Models/Coach.model.js
+++ Models/Coach.model.js
@@ -4,14 +4,14 @@ const Schema=mongoose.Schema
 const Coach=Schema({
     creatorName:{type:String,required:true},
     followers:{type:Number,required:true},
-    channelName:{type:String,required:true},
     earned:{type:Double,required:true},
     email:{type:String,required:true},
     password:{type:String,required:true},
     Channel:{type:[mongoose.Schema.Types.ObjectId]},
-    earningCount:{type:Double},
+    // earningCount:{type:Double},
+    accessToken:{type:String,}
     
 
 })
-const Creator=mongoose.model("Creator",Coach)
+const Creator=new mongoose.model("Creator",Coach)
 module.exports=Creator
