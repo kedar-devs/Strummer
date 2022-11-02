562f811c3cd54106d98a570c2839332c98ae7a91
kedar_devs <kedard249.kd@gmail.com>
2022-11-02 10:56:01 +0530

    Feat:Add Login and become creator COntroller

--- Controller/User.Controller.js
+++ Controller/User.Controller.js
@@ -2,6 +2,7 @@ const { ObjectId } = require("mongodb")
 const UserData = require("../Models/User.model")
 const jwt=require('json-web-token')
 const bcrypt=require('bcryptjs')
+const Creator = require("../Models/Coach.model")
 
 exports.RegisterUser=async(req,res)=>{
     const User={
@@ -12,7 +13,7 @@ exports.RegisterUser=async(req,res)=>{
         contact:req.body.contact,
         ProfilePic:req.body.Pp,
         isCoach:false,
-        coachId:new ObjectId(),
+        CoachId:new ObjectId(),
         accessToken:' ',
         refreshToken:' '
     }
@@ -21,7 +22,7 @@ exports.RegisterUser=async(req,res)=>{
         return res.status(401).send({message:'A user with this email ID already exist'})
     }
 
-    const NewUser=await new UserData({User})
+    const NewUser=await new UserData(User)
     const hash=await bcrypt.hashSync(NewUser.password,10)
     if(!hash){
         return res.status(401).message({message:'Error occured when encrypting the password'})
@@ -45,6 +46,60 @@ exports.RegisterUser=async(req,res)=>{
 
 }
 exports.LoginUser=async(req,res)=>{
+    const {email,password}=req.body
+    const FoundUser=await UserData.findOne({email})
+    if(FoundUser){
+        let PasswordResult=bcrypt.compareSync(password,FoundUser.password)
+        console.log(PasswordResult)
+        if(PasswordResult){
+            let payload={subject:new ObjectId()}
+            let token=await jwt.sign(payload,process.env.SECRET_KEY)
+            FoundUser.accessToken=token
+            FoundUser.save((err,user)=>{
+                if(err){
+                    return res.status(401).send({err})
+                }
+                else{
+                    return res.status(200).send({token})
+                }
+            })
+        }
+
+    }
+    else{
+        return res.status(404).send({message:'User not found'})
+    }
 }
 exports.BecomeCreator=async(req,res)=>{ 
+    const FoundUser=await UserData.findOne({accessToken:req.body.accessToken})
+    const Coach={
+        creatorName:req.body.name?req.body.name:FoundUser.name,
+        followers:0,
+        earned:0.0,
+        email:FoundUser.email,
+        password:req.body.password?req.body.password:FoundUser.password,
+        channel:[],
+        accessToken:' '
+    }
+    let payload={subject:new ObjectId()}
+    let token=await jwt.sign(payload,process.env.SECRET_KEY_COACH)
+    Coach.accessToken=token
+    const NewCoach=await Creator(Coach)
+    NewCoach.save((err,coach)=>{
+        if(err){
+            return res.status(400).send({err})
+        }
+        else{
+            FoundUser.isCoach=true
+            FoundUser.CoachID=coach._id
+            FoundUser.save((err,user)=>{
+                if(err){
+                    return res.status(400).send({err})
+                }
+                return res.status(200).send({token})
+            })
+        
+        }
+    })  
 }
+
