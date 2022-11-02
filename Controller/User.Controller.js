260655652920c4dd9926dca200f459b98b976690
kedar_devs <kedard249.kd@gmail.com>
2022-11-02 12:03:58 +0530

    Feat:Add login with OTP controller code

--- Controller/User.Controller.js
+++ Controller/User.Controller.js
@@ -3,6 +3,12 @@ const UserData = require("../Models/User.model")
 const jwt=require('json-web-token')
 const bcrypt=require('bcryptjs')
 const Creator = require("../Models/Coach.model")
+const Vonage = require('@vonage/server-sdk')
+const otpGenerator = require('otp-generator')
+const vonage = new Vonage({
+  apiKey:"",
+  apiSecret:""
+})
 
 exports.RegisterUser=async(req,res)=>{
     const User={
@@ -11,6 +17,7 @@ exports.RegisterUser=async(req,res)=>{
         password:req.body.password,
         courses:[],
         contact:req.body.contact,
+        otp:12234,
         ProfilePic:req.body.Pp,
         isCoach:false,
         CoachId:new ObjectId(),
@@ -70,6 +77,56 @@ exports.LoginUser=async(req,res)=>{
         return res.status(404).send({message:'User not found'})
     }
 }
+exports.RegisterUserMobile=async(req,res)=>{
+    const {contact,accessToken}=req.body
+    const Otp=otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
+    const from = "Strummer"
+    const to = contact
+    const text = `Your One-Time-Password is ${Otp}
+                    We welcome you to our platform 
+                    Hope you enjoy it`
+    vonage.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+        console.log(err);
+    } else {
+   if(responseData.messages[0]['status'] === "0") {
+      const FoundUser=UserData.findOne({accessToken})
+      FoundUser.otp=Otp
+      FoundUser.save((err,user)=>{
+        if(err){
+            return res.status(400).send({err})
+        }
+        else{
+            return res.status(200).send({message:"Message sent successfully."})
+        }
+      })
+
+    } else {
+   return res.status(400).send(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+   }
+  }
+ })
+}
+exports.LoginUserMobile=async(req,res)=>{
+    const {contact,otp}=req.body
+    const FoundUser=await UserData.find()
+    .Where("contact")
+    .equals(contact)
+    .Where("otp")
+    .equals(otp)
+    let payload={subject:new ObjectId()}
+    let token=await jwt.sign(payload,process.env.SECRET_KEY)
+    FoundUser.accessToken=token
+    FoundUser.save((err,user)=>{
+        if(err){
+            return res.status(400).send({err})
+        }
+        else{
+            return res.status(200).send({token})
+        }
+    })
+}
+
 exports.BecomeCreator=async(req,res)=>{ 
     const FoundUser=await UserData.findOne({accessToken:req.body.accessToken})
     const Coach={
@@ -102,4 +159,20 @@ exports.BecomeCreator=async(req,res)=>{
         }
     })  
 }
+exports.EditProfile=async(req,res)=>{
+    const {name,ProfilePic}=req.body
+    const FoundUser=UserData.findOne({accessToken:req.body.accessToken})
+    FoundUser.name=name?name:FoundUser.name
+    FoundUser.ProfilePic=ProfilePic?ProfilePic:FoundUser.ProfilePic
+    FoundUser.save((err,user)=>{
+        if(err){
+            return res.status(400).send({err})
+        }
+        else{
+            return res.status(200).send({message:'User Details Updated Succesfully'})
+        }
+    })
+}
+
+
 
