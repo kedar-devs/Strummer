5302e15d2eb528f04dc14bb5945f0bd810e15d65
kedar_devs <kedard249.kd@gmail.com>
2022-11-08 12:10:14 +0530

    Feat:Add Comment add,like and dislike feature

---
+++ Controller/Comment.controller.js
@@ -0,0 +1,96 @@
+const UserData = require("../Models/User.model")
+const CommentData = require("../Models/VideoRelatedStuff/Comment.model")
+
+exports.AddComment=async(req,res)=>{
+    const {comment,commentorID}=req.body
+    const FoundUser=await UserData.findOne({_id:commentorID})
+    const Comment={
+        comment:comment,
+        commentorName:FoundUser.name,
+        commmentDp:FoundUser.ProfilePic,
+        likeCount:0,
+        disLikeCount:0,
+        replyCount:0
+    }
+    const NewComment=new CommentData({Comment})
+    NewComment.save((err,user)=>{
+        if(err){
+            return res.status(400).send(err)
+        }
+        else{
+            return res.status(200).send({message:'Comment Added Sucessfully'})
+        }
+    })
+
+}
+exports.IncreaseCount=async(req,res)=>{
+    const {id}=req.body
+    const FoundComment=await CommentData.findOne({_id:id})
+    FoundComment.replyCount+=1
+    FoundComment.save((err,user)=>{
+        if(err){
+            return res.status(400).send(err)
+        }
+        else{
+            return res.status(200).send({message:'Count Increased Succesfully'})
+        }
+    })
+}
+exports.AddLike=async(req,res)=>{
+    const {id}=req.body
+    const FoundComment=await CommentData.findOne({_id:id})
+    FoundComment.likeCount+=1
+    FoundComment.save((err,user)=>{
+        if(err){
+            return res.status(400).send(err)
+        }
+        else{
+            return res.status(200).send({message:'Count Increased Succesfully'})
+        }
+    })
+}
+exports.AddDislike=async(req,res)=>{
+    const {id}=req.body
+    const FoundComment=await CommentData.findOne({_id:id})
+    FoundComment.disLikeCount+=1
+    FoundComment.save((err,user)=>{
+        if(err){
+            return res.status(400).send(err)
+        }
+        else{
+            return res.status(200).send({message:'Count Increased Succesfully'})
+        }
+    })
+}
+exports.RemoveLike=async(req,res)=>{
+    const {id}=req.body
+    const FoundComment=await CommentData.findOne({_id:id})
+    if(FoundComment.likeCount==0){
+        return res.status(200).send({message:'Count cant be increased'}) 
+    }
+    FoundComment.likeCount-=1
+    FoundComment.save((err,user)=>{
+        if(err){
+            return res.status(400).send(err)
+        }
+        else{
+            return res.status(200).send({message:'Count decreased Succesfully'})
+        }
+    })
+}
+exports.RemoveDisLike=async(req,res)=>{
+    const {id}=req.body
+    const FoundComment=await CommentData.findOne({_id:id})
+    if(FoundComment.disLikeCount==0){
+        return res.status(200).send({message:'Count cant be increased'}) 
+    }
+    FoundComment.disLikeCount-=1
+    FoundComment.save((err,user)=>{
+        if(err){
+            return res.status(400).send(err)
+        }
+        else{
+            return res.status(200).send({message:'Count decreased Succesfully'})
+        }
+    })
+}
