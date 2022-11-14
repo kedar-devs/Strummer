da40ca1833520b1dec6a6d6908a74273b973fcde
kedar_devs <kedard249.kd@gmail.com>
2022-11-14 12:10:02 +0530

    Fix:Change ENV variables for cloudinary

--- Helper/Cloudinary.js
+++ Helper/Cloudinary.js
@@ -1,7 +1,8 @@
 const cloudinary=require("cloudinary").v2
+require('dotenv').config()
 cloudinary.config({
     cloud_name:'dwxxqd2zu',
     api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_ENV
+    api_secret:process.env.CLOUDINARY_API_SECRET
 })
 module.exports=cloudinary
