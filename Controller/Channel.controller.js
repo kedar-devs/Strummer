1f8d341ed1b2c9daffd52358dbcea74525b4f919
kedar_devs <kedard249.kd@gmail.com>
2022-11-16 13:19:18 +0530

    Fix:Add try catch block and add func to get Channel

--- Controller/Channel.controller.js
+++ Controller/Channel.controller.js
@@ -1,6 +1,12 @@
-const ChannelData=require('./../Models/content.model')
+const ChannelData=require('./../Models/Channel.model')
 exports.AddChannel=async(req,res)=>{
+    console.log(req.body)
+    try{
     const {channelName,about,channelCreator}=req.body
+    const FoundChannel=await ChannelData.findOne({channelName})
+    if(FoundChannel){
+        return res.status(400).send({message:'A user with that Name Already exist'})
+    }
     const channel={
         channelName:channelName,
         channelSubCount:0,
@@ -11,8 +17,9 @@ exports.AddChannel=async(req,res)=>{
         socialTwit:req.body.socialTwit?req.body.socialTwit:' ',
         channelCreator:channelCreator
     }
-    const NewChannel=new CharacterData(channel)
+    const NewChannel=new ChannelData(channel)
     NewChannel.save((err,user)=>{
+        console.log(user)
         if(err){
             return res.status(200).send(err)
         }
@@ -20,9 +27,13 @@ exports.AddChannel=async(req,res)=>{
             return res.status(200).send({message:'Channel Added Succesfully'})
         }
     })
+}catch{
+    return res.status(400).send({message:err})
+}
 }
 exports.editChannelName=async(req,res)=>{
     const {_id,newName}=req.body
+    try{
     const FoundChannel=await ChannelData.findOne({_id})
     FoundChannel.channelName=newName
     FoundChannel.save((err,user)=>{
@@ -33,9 +44,12 @@ exports.editChannelName=async(req,res)=>{
             return res.status(200).send({message:'ChannelName Updated Succesfully'})
         }
     })
+}catch(err){
+    return res.status(400).send(err)
+}
 }
 exports.AddSubscriber=async(req,res)=>{
-    const {_id}=req.body
+    const {_id}=req.params
     const FoundChannel=await ChannelData.findOne({_id})
     FoundChannel.channelSubCount+=1
     FoundChannel.save((err,user)=>{
@@ -48,7 +62,7 @@ exports.AddSubscriber=async(req,res)=>{
     })
 }
 exports.removeSubscriber=async(req,res)=>{
-    const {_id}=req.body
+    const {_id}=req.params
     const FoundChannel=await ChannelData.findOne({_id})
     if(FoundChannel.channelSubCount>0){
     FoundChannel.channelSubCount-=1
@@ -76,4 +90,33 @@ exports.editSocial=async(req,res)=>{
             return res.status(200).send({message:'Subscriber Removed Succesfully'})
         }
     })
-}   
+} 
+exports.GetAllChannel=async(req,res)=>{
+    const AllChannel=await ChannelData.find()
+    if(AllChannel){
+        return res.status(200).send(AllChannel)
+    }
+    else{
+        return res.status(400).send({message:'No User Found'})
+    }
+}  
+exports.getOneChannel=async(req,res)=>{
+    const _id=req.params.id
+    const OneChannel=await ChannelData.findOne({_id})
+    if (OneChannel){
+        return res.status(200).send(OneChannel)
+    }
+    else{
+        return res.status(400).send({message:'No user with that id found'})
+    }
+}
+exports.getCreatorChannel=async(req,res)=>{
+    const channelCreator=req.params.creator
+    const FoundChannel=await ChannelData.find({channelCreator})
+    if(FoundChannel){
+        return res.status(200).send(FoundChannel)
+    }
+    else{
+        return res.status(400).send({message:'User no found'})
+    }
+}
