d5d5b38aa2e4d971b424649b8b8f040c124bd5db
kedar_devs <kedard249.kd@gmail.com>
2022-11-16 13:09:34 +0530

    Fix:Make channel Name Unique and add Parent Id

--- Models/Coach.model.js
+++ Models/Coach.model.js
@@ -2,12 +2,13 @@ const { Double } = require('mongodb')
 const mongoose=require('mongoose')
 const Schema=mongoose.Schema
 const Coach=Schema({
-    creatorName:{type:String,required:true},
+    creatorName:{type:String,required:true,unique:true},
     followers:{type:Number,required:true},
     earned:{type:Schema.Types.Decimal128,required:true},
     email:{type:String,required:true},
     password:{type:String,required:true},
     Channel:{type:[mongoose.Schema.Types.ObjectId]},
+    parentId:{type:mongoose.Schema.Types.ObjectId},
     // earningCount:{type:Double},
     accessToken:{type:String,}
     
