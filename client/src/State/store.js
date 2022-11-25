7aeab68ded603fb9b96e0efe7b5fedfcbfcc2d3d
kedar_devs <kedard249.kd@gmail.com>
2022-11-25 19:05:09 +0530

    Feat:Create Store for action and reducer

---
+++ client/src/State/store.js
@@ -0,0 +1,6 @@
+import { applyMiddleware, createStore } from "redux";
+import thunk from 'redux-thunk'
+import Reducers from "./Reducers";
+
+const Store=createStore(Reducers,{},applyMiddleware(thunk))
+export default Store
