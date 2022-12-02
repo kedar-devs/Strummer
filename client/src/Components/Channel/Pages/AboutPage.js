eae149cd9d0c033df238501e29b4f7db61e58edf
kedar_devs <kedard249.kd@gmail.com>
2022-12-02 12:16:46 +0530

    Feat:Add Playlist video community and about page

---
+++ client/src/Components/Channel/Pages/AboutPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div className='grid grid-cols-6'>
+        <div>
+            Description:
+            
+        </div>
+        <div>
+            stats
+        </div>
+    </div>
+  )
+}
+
+export default AboutPage
