3333e44a31fb682f368021a61dc75a6cd91d7ddd
kedar_devs <kedard249.kd@gmail.com>
2022-12-05 11:24:14 +0530

    Feat:Add title, description,tags to the content model

--- Models/content.model.js
+++ Models/content.model.js
@@ -4,6 +4,11 @@ const mongoose=require('mongoose')
 const Schema=mongoose.Schema
 const ContentSchema=Schema({
     ContentUrl:{type:String,required:true},
+    ImageThumbnail:{type:String,required:true},
+    Title:{type:String,required:true},
+    Description:{type:String,required:true},
+    createAt:{type:Date,required:true},
+    Tags:{type:[String],required:true},
     ContentCreator:{type:mongoose.Schema.Types.ObjectId,required:true},
     channelId:{type:mongoose.Schema.Types.ObjectId,required:true},
     LikeCount:{type:Number},
