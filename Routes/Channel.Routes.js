f208f36fc6807cf6ab030b24b8182c0201f0f240
kedar_devs <kedard249.kd@gmail.com>
2022-12-09 12:52:03 +0530

    Feat:Add routes to edit channel image and search via tags

--- Routes/Channel.Routes.js
+++ Routes/Channel.Routes.js
@@ -1,6 +1,7 @@
 const ChannelController=require('./../Controller/Channel.controller')
 const routes=require('express').Router()
 routes.post("/AddChannel",ChannelController.AddChannel)
+routes.put("/editImage/:id",ChannelController.editChannelImage)
 routes.get("/AddSubscriber/:_id",ChannelController.AddSubscriber)
 routes.put("/EditChannelName",ChannelController.editChannelName)
 routes.get("/RemoveSubscriber/:_id",ChannelController.removeSubscriber)
