5c4729bc206062fdcfa5f6483200546ca23079de
kedar_devs <kedard249.kd@gmail.com>
2022-12-15 12:14:21 +0530

    Feat:Add react icons and style the video page

--- client/src/Components/VideoPlayer/VideoPlayerPage.js
+++ client/src/Components/VideoPlayer/VideoPlayerPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import AddComment from "./AddComment";
 import Comment from "./Comment";
+import { AiFillHeart,AiOutlineMenu } from 'react-icons/ai';
+import {FaHeartBroken,FaShare} from 'react-icons/fa'
+import {ImDownload3} from 'react-icons/im'
 
 function VideoPlayerPage() {
   return (
@@ -11,23 +14,27 @@ function VideoPlayerPage() {
           <div className="grid grid-cols-4 mt-3">
             <div className="grid grid-cols-4 col-span-2">
               <div className="">
+              <div className="justify-end">
                 <img
                   src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
                   alt="..."
-                  className="shadow rounded-full max-w-full h-auto align-middle border-none"
+                  className="shadow rounded-full max-w-full h-auto align-middle border-none items-end w-2/6 object-right"
                 />
               </div>
-              <div className="item-left col-span-2">channel Name</div>
-              <div className="">
-                <button>Follow</button>
+              </div>
+              <div className="justify-items-start text-xl col-span-2">channel Name</div>
+              <div className="text-xl">
+                <button className="rounded-full border bg-black px-5 py-1">Follow</button>
               </div>
             </div>
-            <div className="col-span-2 items-center">
-              <button>Like</button>
-              <button>dislike</button>
-              <button>Share</button>
-              <button>Download</button>
-              <button>more</button>
+            <div className="col-span-2 items-center text-xl">
+              <button className="rounded-lg border bg-black">
+              <button className="ml-4 text-red-700 "><AiFillHeart /></button>
+              <button className="ml-3 text-red-700 p-3 border-l-2"><FaHeartBroken /></button>
+              </button>
+              <button className="ml-3 text-blue-600 rounded-lg border p-3 bg-black"><FaShare /></button>
+              <button className="ml-3 text-green-600 rounded-lg border p-3 bg-black"><ImDownload3 /></button>
+              <button className="ml-3 text-white rounded-lg border p-3 bg-black"><AiOutlineMenu /></button>
             </div>
             
           </div>
