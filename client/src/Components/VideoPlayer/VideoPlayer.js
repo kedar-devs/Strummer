e36c8c17102b360940a187369bcb598bf8fcc374
kedar_devs <kedard249.kd@gmail.com>
2022-12-16 16:16:50 +0530

    Feat:Add Videoplayer page to videoPlayer

--- client/src/Components/VideoPlayer/VideoPlayer.js
+++ client/src/Components/VideoPlayer/VideoPlayer.js
@@ -3,6 +3,7 @@ import React,{useState,useEffect} from 'react'
 import { DefaultPlayer as Video } from 'react-html5video';
 import 'react-html5video/dist/styles.css';
 import { useParams } from 'react-router-dom';
+import VideoPlayerPage from './VideoPlayerPage';
 
 function VideoPlayer(props) {
   const [video,setVideoUrl]=useState()
@@ -21,7 +22,9 @@ function VideoPlayer(props) {
   },[id])
   return (
     <div >
-      {loading?<Video autoPlay
+      {loading?
+      <>
+      <Video autoPlay
             className='lg:w-4/5 h-96 lg:ml-32 md:ml-20 sm:w-full md:w-4/5'  
             controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
             poster={video.ImageThumbnail}
@@ -31,7 +34,10 @@ function VideoPlayer(props) {
              
             <source src={video.ContentUrl} type="video/webm" />
            
-        </Video>:<>Loading</>}
+        </Video>
+        <VideoPlayerPage />
+        </>
+        :<>Loading</>}
     </div>
   )
 }
