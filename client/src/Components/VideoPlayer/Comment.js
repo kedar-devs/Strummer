229de16dbcb8570304743c785e055d9e3f2e371e
kedar_devs <kedard249.kd@gmail.com>
2022-12-16 16:16:17 +0530

    Feat:Add css to the comment page and map the comments array

--- client/src/Components/VideoPlayer/Comment.js
+++ client/src/Components/VideoPlayer/Comment.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Comment() {
+  const[comments,setComment]=useState(['Comment1 svfdfbdbgfgfgfhnfhnmm','Comment2','Comment1','Comment2','Comment1','Comment2','Comment1','Comment2','Comment1','Comment2',])
   return (
-    <div className='grid grid-cols-4'>
-        <div className="w-2/12">
-                <img
-                  src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
-                  alt="..."
-                  className="shadow rounded-full max-w-full h-auto align-middle border-none"
-                />
-              </div>
-        <div className='col-span-2'>
-            comment
-        </div>
-
-
-        
-        <div>
-            <button>:</button>
-        </div>
+    <div className='overflow-y-scroll h-64 scrollbar-hide'>
+    {comments.map((comment)=>{
+      return(
+       <div className='grid grid-cols-4 my-2 '>
+       <div className="w-2/6">
+               <img
+                 src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
+                 alt="..."
+                 className="shadow rounded-full max-w-full h-auto align-middle border-none"
+               />
+             </div>
+       <div className='col-span-2'>
+           {comment}
+       </div>
+       <div>
+           <button>:</button>
+       </div>
+   </div>
+      )
+    })
+  }
+   
     </div>
   )
 }
