af049c06e4966ed8c70dfe6ca5a872fef30ccc00
kedar_devs <kedard249.kd@gmail.com>
2022-12-16 16:15:39 +0530

    Feat:Add css to the Add comment page with dynamic buttom

--- client/src/Components/VideoPlayer/AddComment.js
+++ client/src/Components/VideoPlayer/AddComment.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function AddComment() {
+  const [addButton,setAddButton]=useState(false)
+
   return (
-    <div>
-        <input type='text' />
+    <div className='flex'>
+        <input type='text' className='outline-none bg-transparent border-b-2 border-blue-600' placeholder='Add Comment' onClick={()=>{setAddButton(!addButton)}}/>
+        {addButton?<>
+        <button>comment</button>
+        </>:<></>}
     </div>
   )
 }
