8a442b60530ec1f7d919056598bde4f43d1294fd
kedar_devs <kedard249.kd@gmail.com>
2022-12-20 17:08:32 +0530

    Feat:add api call to channel Page

--- client/src/Components/Channel/Pages/ChannelPage.js
+++ client/src/Components/Channel/Pages/ChannelPage.js
@@ -1,19 +1,35 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { Grid } from '@mui/material'
 import DynamicCard from '../../Card/DynamicCard'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 function ChannelPage() {
   const [cardDetails,setCard]=useState(['1','2','3'])
+  const [loader,setLoader]=useState(false)
+  const {id}=useParams()
+  useEffect(()=>{
+      
+      axios.get(`http://localhost:5000/Channel/GetOneChannel/${id}`)
+      .then(result=>{
+        console.log(result.data)
+        setCard(result.data)
+        setLoader(true)
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+  },[id])
   return (
     <div>
-         <Grid container spacing={2}>
+         {loader?<Grid container spacing={2}>
         {cardDetails.map((ele)=>{
             return(
                 <Grid item xs={12} md={4} lg={3} sm={6}>
-                  <DynamicCard/>
+                  <DynamicCard cardDetails={ele}/>
                   </Grid>
             )
         })}
-        </Grid>
+        </Grid>:<>Loader</>}
     </div>
   )
 }
