b0bc5517d730bd3f09db140180a5e7f5ff86b46d
kedar_devs <kedard249.kd@gmail.com>
2022-12-20 17:09:12 +0530

    Feat:add api call to Video Page

--- client/src/Components/Channel/Pages/VideoPage.js
+++ client/src/Components/Channel/Pages/VideoPage.js
@@ -1,19 +1,32 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { Grid } from '@mui/material'
 import DynamicCard from '../../Card/DynamicCard'
+import axios from 'axios'
 function VideoPage() {
   const [cardDetails,setCard]=useState(['1','2','3'])
+  const [loader,setLoader]=useState(false)
+  useEffect(()=>{
+    axios.get("http://localhost:5000/Content/GetContent")
+    .then(result=>{
+      console.log(result.data)
+      setCard(result.data)
+      setLoader(true)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  },[])
   return (
     <div>
-         <Grid container spacing={2}>
+        {loader?<Grid container spacing={2}>
         {cardDetails.map((ele)=>{
             return(
                 <Grid item xs={12} md={4} lg={3} sm={6}>
-                  <DynamicCard/>
+                  <DynamicCard cardDetails={ele}/>
                   </Grid>
             )
         })}
-        </Grid>
+        </Grid>:<>Loader</>}
     </div>
   )
 }
