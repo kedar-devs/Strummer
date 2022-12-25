766b75a23282bbc64583e0776aeda2de7bc4e5ed
kedar_devs <kedard249.kd@gmail.com>
2022-12-25 20:09:13 +0530

    Fix:Add axios request to channel to check if user is a creator or not

--- client/src/Components/Channel/Channel.js
+++ client/src/Components/Channel/Channel.js
@@ -1,15 +1,37 @@
-import React from 'react'
-// import ChannelHeading from './ChannelHeading'
+import React,{useState,useEffect} from 'react'
+import ChannelHeading from './ChannelHeading'
 // import ChannelNavbar from './ChannelNavbar'
 import BecomeCreator from './BecomeCreator';
-import { Outlet } from "react-router-dom";
+
+import axios from 'axios';
 function Channel() {
+  const [isNotCreator,setCreatorBool]=useState(false)
+  const [Creator,setCreator]=useState({})
+  useEffect(()=>{
+    const Token=localStorage.getItem('Token')
+    axios.get(`http://localhost:5000/Creator/GetFromParent/${Token}`)
+    .then(result=>{
+      console.log(result)
+        setCreator(result.data.FoundCreator)
+        setCreatorBool(result.data.isFound)
+    })
+    .catch(err=>{
+      console.log(err)
+      setCreatorBool(err.data.isFound)
+    })
+  },[])
   return (
     <div>
+      {isNotCreator?
+      <div>
+      <ChannelHeading />
+      </div>:
       <BecomeCreator/>
+    }
       {/* <ChannelHeading />
       <ChannelNavbar /> */}
-      <Outlet />
+       {/* <Outlet /> */}
+   
     </div>
   )
 }
