d82b4038d129d7e5e6d30b02a786e4c639e32292
kedar_devs <kedard249.kd@gmail.com>
2022-12-27 18:47:45 +0530

    Fix:Add API request to show channel and your channel

--- client/src/Components/Channel/ShowChannels.js
+++ client/src/Components/Channel/ShowChannels.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Dp from '../../Assets/dance.jpg'
 import {useDispatch} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import { channelActionCreator } from "../../State";
@@ -11,45 +10,49 @@ import axios from "axios";
 //BsPlusCircleDotted
 function ShowChannels(props) {
   const [channels,setChannel] = useState({});
+  const [loading,setLoading]=useState(false)
   const dispatch=useDispatch()
   const action=bindActionCreators(channelActionCreator,dispatch)
   const navigation=useNavigate()
   const selectorData=useSelector(STATE=>STATE.channel)
+  let creatorId=selectorData.creatorId
   useEffect(()=>{
-    let creatorId=selectorData.creatorId
     axios.get(`http://localhost:5000/Channel/GetCreatorsCahnnel/${creatorId}`)
     .then(result=>{
       console.log(result.data)
+      if(result.data!==undefined || result.data!==null || result.data.length===0){
       setChannel(result.data)
+      setLoading(true)
+      }
     })
-  },[])
+  },[creatorId])
   const gotoChannel=(id)=>{
     action.AddChannelId(id)
     navigation(`/YourChannel/${id}`)
   }
   const navigate=()=>{
-   
-    action.AddCreatorId(props.creator[0]._id)
+    console.log(props)
+    action.AddCreatorId(props.creators[0]._id)
     navigation(`/CreateChannel`)
   }
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-12 text-white h-screen" >
-      {channels.map((channel) => {
+      {loading?<>{channels.map((channel) => {
         return (
           <div className="max-w-sm rounded overflow-hidden shadow-lg m-5 h-60" style={{backgroundColor:'#282c34'}} onClick={()=>{gotoChannel(channel._id)}}>
       
             <img
               className="h-44 rounded-full w-44 object-center"
-              src={Dp}
+              src="https://images.unsplash.com/photo-1568861368385-3534aca5cf41?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
               alt="Sunset in the mountains"
             />
           
             <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <div className="font-bold text-xl mb-2">{channel.channelName}</div>
             </div>
           </div>
         );
-      })}
+      })}</>:<></>}
       <div className="max-w-sm rounded overflow-hidden shadow-lg m-5 h-60 " style={{backgroundColor:'#282c34'}} onClick={()=>{navigate()}}>
       <AiOutlinePlus size={120} className='border-2 rounded-full border-dashed'/>
       <div className="px-6 py-4">
--- client/src/Components/Channel/YourChannel.js
+++ client/src/Components/Channel/YourChannel.js
@@ -1,11 +1,30 @@
-import React from "react";
-import ChannelHeading from "./ChannelHeading";
+import React,{useEffect,useState} from "react";
+import YourChannelHeading from "./YourChannelHeading";
 import ChannelNavbar from "./ChannelNavbar";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
 function YourChannel() {
+  const [channelDetail,setChannel]=useState()
+  const selectorData=useSelector(STATE=>STATE.channel)
+  useEffect(()=>{
+    const channelId=selectorData.channelId
+    console.log(channelId)
+    axios.get(`http://localhost:5000/Channel/GetOneChannel/${channelId}`)
+    .then(result=>{
+      console.log(result.data)
+      setChannel(result.data)
+
+    })
+    .catch(err=>{
+      console.log(err)
+
+    })
+  },[selectorData])
   return (
     <div>
-      <ChannelHeading />
+      <YourChannelHeading channelInfo={channelDetail} />
       <ChannelNavbar />
       <Outlet />
     </div>
