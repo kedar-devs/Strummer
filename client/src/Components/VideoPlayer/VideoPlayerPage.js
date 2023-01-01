7a1be24b2cf9a88e9447588ce5c2115f01dea644
kedar_devs <kedard249.kd@gmail.com>
2023-01-01 21:38:10 +0530

    Fix:Add Routes to like dislike share and set videoInfo

--- client/src/Components/VideoPlayer/VideoPlayerPage.js
+++ client/src/Components/VideoPlayer/VideoPlayerPage.js
@@ -1,17 +1,84 @@
-import React from "react";
+import React,{useState} from "react";
 import AddComment from "./AddComment";
 import Comment from "./Comment";
 import { AiFillHeart, AiOutlineMenu } from "react-icons/ai";
 import { FaHeartBroken, FaShare } from "react-icons/fa";
 import { ImDownload3 } from "react-icons/im";
+import axios from "axios";
 
 function VideoPlayerPage(props) {
-  console.log('From Video',props)
+  console.log(props)
+  const [videoInfo,setVideoInfo]=useState(props.videoInfo)
+  const [isLiked,setisLiked]=useState(false)
+  const [isDisLiked,setIsDisliked]=useState(false)
+  const AddLike=(id)=>{
+    if(!isLiked){
+    axios.get(`http://localhost:5000/Content/AddLike/${id}`)
+    .then(result=>{
+      console.log(result.data)
+      let newVideoInfo=videoInfo
+      newVideoInfo.LikeCount=result.data.newLikeCount
+      setVideoInfo(newVideoInfo)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    setisLiked(!isLiked)
+  }
+  else{
+    axios.get(`http://localhost:5000/Content/RemoveLike/${id}`)
+    .then(result=>{
+      console.log(result.data)
+      let newVideoInfo=videoInfo
+      newVideoInfo.LikeCount=result.data.newLikeCount
+      setVideoInfo(newVideoInfo)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    setisLiked(!isLiked)
+  }
+
+  }
+  const AddDisLike=(id)=>{
+    if(!isDisLiked){
+    axios.get(`http://localhost:5000/Content/AddDislikes/${id}`)
+    .then(result=>{
+      console.log(result)
+      let newVideoInfo=videoInfo
+      newVideoInfo.DislikeCount=result.data.newDisLikeCount
+      setVideoInfo(newVideoInfo)
+      console.log(videoInfo)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    setIsDisliked(!isDisLiked)
+  }
+  else{
+    axios.get(`http://localhost:5000/Content/RemoveDisLike/${id}`)
+    .then(result=>{
+      console.log(result)
+      let newVideoInfo=videoInfo
+      newVideoInfo.DislikeCount=result.data.newDisLikeCount
+      setVideoInfo(newVideoInfo)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    setIsDisliked(!isDisLiked)
+  }
+  }
+  const Share=()=>{
+    const url=window.location.href
+    navigator.clipboard.writeText(url)
+    alert('URL has been Copied and ready to be shared')
+  }
   return (
     <div className="h-80">
       <div className="grid grid-cols-3 gap-2 items-left">
         <div className="col-span-2 text-white justify-start ml-16">
-          <h1 className="text-2xl ">Something exciting here</h1>
+          <h1 className="text-2xl ">{videoInfo.Title}</h1>
           <div className="grid grid-cols-4 mt-3">
             <div className="grid grid-cols-4 col-span-2">
               <div className="justify-end object-right ">
@@ -35,15 +102,21 @@ function VideoPlayerPage(props) {
               </div>
             </div>
             <div className="col-span-2 items-center text-xl">
-              <button className="rounded-lg border bg-black">
-                <button className="ml-4 text-red-700 ">
-                  <AiFillHeart />
+              <button className="rounded-lg border text-sm bg-black">
+                <button className="ml-4  m-0 mt-0 text-red-700 " onClick={()=>{AddLike(videoInfo._id)}}>
+                  <AiFillHeart size={14}/>
+                  <p className="text-xs">
+                  {videoInfo.LikeCount}
+                  </p>
                 </button>
-                <button className="ml-3 text-red-700 p-3 border-l-2">
-                  <FaHeartBroken />
+                <button className="ml-3  m-0 mt-0 text-red-700 p-3 border-l-2" onClick={()=>{AddDisLike(videoInfo._id)}}>
+                  <FaHeartBroken size={14}/>
+                  <p className="text-xs">
+                  {videoInfo.DislikeCount}
+                  </p>
                 </button>
               </button>
-              <button className="ml-3 text-blue-600 rounded-lg border p-3 bg-black">
+              <button className="ml-3 text-blue-600 rounded-lg border p-3 bg-black" onClick={()=>{Share()}}>
                 <FaShare />
               </button>
               <button className="ml-3 text-green-600 rounded-lg border p-3 bg-black">
@@ -54,26 +127,12 @@ function VideoPlayerPage(props) {
               </button>
             </div>
           </div>
-          European Parliament's Vice-President Eva Kaili has been arrested on
-          charges of corruption and money-laundering. Kaili is accused of
-          accepting money and gifts from Qatar in exchange of influencing
-          European policy in Doha's favour. @MollyGambhir tells you.
-          #europeanparliament #evakaili #gravitas About Channel: WION The World
-          is One News, examines global issues with in-depth analysis. We provide
-          much more than the news of the day. Our aim to empower people to
-          explore their world. With our Global headquarters in New Delhi, we
-          bring you news on the hour, by the hour. We deliver information that
-          is not biased. We are journalists who are neutral to the core and
-          non-partisan when it comes to the politics of the world. People are
-          tired of biased reportage and we stand for a globalised united world.
-          So for us the World is truly One. Please keep discussions on this
-          channel clean and respectful and refrain from using racist or sexist
-          slurs as well as personal insults.
+          {videoInfo.Description}
         </div>
-        <div className="text-white">
+        <div className="text-white mt-10">
           {" "}
-          <AddComment id={props.videoInfo._id}/>
-          <Comment />
+          <AddComment id={videoInfo._id}/>
+          <Comment id={videoInfo._id}/>
         </div>
       </div>
     </div>
