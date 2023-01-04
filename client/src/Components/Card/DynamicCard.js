28f53f6478372ce2921419aeb15f3e74b01a1a8b
kedar_devs <kedard249.kd@gmail.com>
2023-01-04 13:11:40 +0530

    Fix:Add History routes to the dyanmic card and all it onClick

--- client/src/Components/Card/DynamicCard.js
+++ client/src/Components/Card/DynamicCard.js
@@ -9,7 +9,7 @@ import CardActions from '@mui/material/CardActions';
 // import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
-
+import axios from 'axios'
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
@@ -17,6 +17,30 @@ import ShareIcon from '@mui/icons-material/Share';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 function DynamicCard(props) {
   const navigator=useNavigate()
+  const AddHistory=(id)=>{
+    
+    let Token=localStorage.getItem('Token')
+    console.log('from history',id,Token)
+    if(Token){
+      axios.get(`http://localhost:5000/User/GetUserId/${Token}`)
+      .then(result=>{
+        console.log(result.data)
+        const userId=result.data
+        const contentId=id
+        axios.post('http://localhost:5000/Content/Video/AddHistory',{userId,contentId})
+        .then(result=>{
+          console.log(result.data)
+        })
+        .catch(err=>{
+          console.log(err)
+        })
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    }
+    navigator(`/VideoPlayer/${id}`)
+  }
   // const [expanded, setExpanded] = React.useState(false);
 
   // const handleExpandClick = () => {
@@ -24,7 +48,7 @@ function DynamicCard(props) {
   // }
   return (
     <div className='max-w-sm rounded-lg overflow-hidden shadow-md shadow-blue-300' >
-      <Card className='h-96' onClick={()=>{navigator(`/VideoPlayer/${props.cardDetail._id}`)}}>
+      <Card className='h-96' onClick={()=>{AddHistory(props.cardDetail._id)}}>
        <CardMedia
         component="img"
         className='rounded-t-lg h-60'
@@ -44,7 +68,7 @@ function DynamicCard(props) {
           </IconButton>
         }
         title={props.cardDetail.Title}
-        subheader={props.cardDetail.Description}
+        subheader={props.cardDetail.Description.slice(0,50)+'....'}
       />
        <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
