6685b0cfa42a20ddc65d78f27a6e28bb2905755a
kedar_devs <kedard249.kd@gmail.com>
2023-01-08 22:25:01 +0530

    Fix:Add API call inside Video and channel page

--- client/src/Components/Channel/Pages/ChannelPage.js
+++ client/src/Components/Channel/Pages/ChannelPage.js
@@ -1,15 +1,16 @@
 import React,{useState,useEffect} from 'react'
 import { Grid } from '@mui/material'
 import DynamicCard from '../../Card/DynamicCard'
-import { useParams } from 'react-router-dom'
+
 import axios from 'axios'
+import { useOutletContext } from 'react-router-dom'
 function ChannelPage() {
   const [cardDetails,setCard]=useState(['1','2','3'])
   const [loader,setLoader]=useState(false)
-  const {id}=useParams()
+  const [channelDetails]=useOutletContext()
   useEffect(()=>{
       
-      axios.get(`http://localhost:5000/Channel/GetOneChannel/${id}`)
+      axios.get(`http://localhost:5000/Channel/GetAllCreatorChannel/${channelDetails._id}`)
       .then(result=>{
         console.log(result.data)
         setCard(result.data)
@@ -18,7 +19,7 @@ function ChannelPage() {
       .catch(err=>{
         console.log(err)
       })
-  },[id])
+  },[channelDetails])
   return (
     <div>
          {loader?<Grid container spacing={2}>
--- client/src/Components/Channel/Pages/VideoPage.js
+++ client/src/Components/Channel/Pages/VideoPage.js
@@ -2,11 +2,14 @@ import React,{useState,useEffect} from 'react'
 import { Grid } from '@mui/material'
 import DynamicCard from '../../Card/DynamicCard'
 import axios from 'axios'
+import { useOutletContext } from 'react-router-dom'
 function VideoPage() {
-  const [cardDetails,setCard]=useState(['1','2','3'])
+  const [channelDetails] =useOutletContext()
+  console.log(channelDetails)
+  const [cardDetails,setCard]=useState([])
   const [loader,setLoader]=useState(false)
   useEffect(()=>{
-    axios.get("http://localhost:5000/Content/GetContent")
+    axios.get(`http://localhost:5000/Content/GetChannel/${channelDetails._id}`)
     .then(result=>{
       console.log(result.data)
       setCard(result.data)
@@ -15,14 +18,14 @@ function VideoPage() {
     .catch(err=>{
       console.log(err)
     })
-  },[])
+  },[channelDetails])
   return (
     <div>
         {loader?<Grid container spacing={2}>
         {cardDetails.map((ele)=>{
             return(
                 <Grid item xs={12} md={4} lg={3} sm={6}>
-                  <DynamicCard cardDetails={ele}/>
+                  <DynamicCard cardDetail={ele}/>
                   </Grid>
             )
         })}
