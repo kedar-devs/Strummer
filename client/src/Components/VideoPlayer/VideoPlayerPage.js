1808c0954b1ed29a6c818bae8a91cb48efb67dc7
kedar_devs <kedard249.kd@gmail.com>
2023-01-15 21:25:21 +0530

    Feat:Add Liked Video and dislike backend Routes to videoPlayerPage

--- client/src/Components/VideoPlayer/VideoPlayerPage.js
+++ client/src/Components/VideoPlayer/VideoPlayerPage.js
@@ -3,7 +3,8 @@ import AddComment from "./AddComment";
 import Comment from "./Comment";
 import { AiFillHeart, AiOutlineMenu } from "react-icons/ai";
 import { FaHeartBroken, FaShare } from "react-icons/fa";
-import { ImDownload3 } from "react-icons/im";
+import { BiDollar } from "react-icons/bi";
+import StripeCheckout from 'react-stripe-checkout';
 import axios from "axios";
 
 function VideoPlayerPage(props) {
@@ -13,7 +14,12 @@ function VideoPlayerPage(props) {
   const [isLiked,setisLiked]=useState(false)
   const [isDisLiked,setIsDisliked]=useState(false)
   const [isFollowing,setFollowing]=useState(false)
-
+  const [UsersId,setUserId]=useState('')
+  const prod={
+    name:'creator',
+    email:'kedard249.kd@gmail.com',
+    price:100
+}
   useEffect(()=>{
     setVideoInfo(props.videoInfo)
     setChannelInfo(props.channelInfo)
@@ -23,6 +29,7 @@ function VideoPlayerPage(props) {
         .then(user=>{
           console.log(user.data)
           const userId=user.data
+          setUserId(userId)
           const channelId=props.channelInfo._id
           axios.post('http://localhost:5000/Channel/CheckSubscription',{userId,channelId})
           .then(subStatus=>{
@@ -39,7 +46,19 @@ function VideoPlayerPage(props) {
     setLoading(false)
   },[videoInfo,props])
 
-
+  const makePayment=(token)=>{
+    const body={
+        token,
+        prod
+    }
+    axios.post('http://localhost:5000/User/payment',body)
+    .then(res=>{
+        console.log(res)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+}
   const FollowChannel=(id)=>{
     const Token=localStorage.getItem('Token')
     if(Token){
@@ -73,6 +92,17 @@ function VideoPlayerPage(props) {
     .catch(err=>{
       console.log(err)
     })
+    const body={
+      ContentId:id,
+      userId:UsersId
+    }
+    axios.post('http://localhost:5000/Content/Likes/AddLikes',body)
+    .then(result=>{
+      console.log(result.data)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
     setisLiked(!isLiked)
  
   }
@@ -83,6 +113,17 @@ function VideoPlayerPage(props) {
       let newVideoInfo=videoInfo
       newVideoInfo.LikeCount=result.data.newLikeCount
       setVideoInfo({...videoInfo,LikeCount:result.data.newLikeCount})
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    const body={
+      ContentId:id,
+      userId:UsersId
+    }
+    axios.delete('http://localhost:5000/Content/Likes/DeleteLikes',{data:body})
+    .then(result=>{
+      console.log(result.data)
     })
     .catch(err=>{
       console.log(err)
@@ -106,6 +147,7 @@ function VideoPlayerPage(props) {
     .catch(err=>{
       console.log(err)
     })
+
     setIsDisliked(!isDisLiked)
   
   }
@@ -182,7 +224,15 @@ function VideoPlayerPage(props) {
                 <FaShare />
               </button>
               <button className="ml-3 text-green-600 rounded-lg border p-3 bg-black">
-                <ImDownload3 />
+              <StripeCheckout
+                stripeKey="pk_test_51MOxzRSHgs3sudxeetSkWvkv7n6rPjksnAej8B4liQdNstnZWEpJ623mm3qYPcWMKUv8yR4rbf3xWYWgMz4NSWUo00GOD205wX"
+                token={makePayment}
+                name='Reward Creator'
+                amount={100}
+                key={videoInfo._id} 
+                >
+                  <BiDollar />
+                  </StripeCheckout>
               </button>
               <button className="ml-3 text-white rounded-lg border p-3 bg-black">
                 <AiOutlineMenu />
