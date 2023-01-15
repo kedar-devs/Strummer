2311241034ea6f649080b980c3b8c96074e0adeb
kedar_devs <kedard249.kd@gmail.com>
2023-01-15 21:24:28 +0530

    Feat:Add Liked Video Page

---
+++ client/src/Components/LikedVideo/LikedVideo.js
@@ -0,0 +1,50 @@
+import React,{useState,useEffect} from 'react'
+import { Grid } from "@mui/material";
+import DynamicCard from "../Card/DynamicCard";
+import axios from "axios";
+function LikedVideo() {
+    const [cardDetails, setCard] = useState([]);
+    const [loading,setLoader]=useState(true)
+    const [isData,setIsData]=useState(false)
+    useEffect(()=>{
+        const Token=localStorage.getItem('Token')
+        if(Token){
+            axios.get(`http://localhost:5000/User/GetUserId/${Token}`)
+            .then(result=>{
+                console.log(result.data)
+                const id=result.data
+                axios.get(`http://localhost:5000/Content/likes/getLikedVideos/${id}`)
+                .then(result=>{
+                    console.log(result.data.result)
+                    setCard(result.data.result)
+                    setIsData(true)
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+                setLoader(false)
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+        }
+      },[])
+  return (
+    <div className="h-screen">
+    {loading?<>Loading</>:
+    <>
+    {isData?
+  <Grid container spacing={2}>
+    {cardDetails.map((card) => {
+      return (
+        <Grid item xs={12} md={4} lg={3} sm={6}>
+          <DynamicCard cardDetail={card}/>
+        </Grid>
+      );
+    })}
+  </Grid>:<></>}</>}
+</div>
+  )
+}
+
+export default LikedVideo
