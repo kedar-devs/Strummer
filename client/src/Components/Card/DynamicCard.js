29f49b82df43044e6f68422daa98719f5f789c40
kedar_devs <kedard249.kd@gmail.com>
2023-01-16 21:21:39 +0530

    Feat:Add ability to like from card itself

--- client/src/Components/Card/DynamicCard.js
+++ client/src/Components/Card/DynamicCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 // import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
@@ -10,17 +10,40 @@ import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import axios from 'axios'
+import {BsHeartFill} from 'react-icons/bs'
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 function DynamicCard(props) {
-  const navigator=useNavigate()
+  const [isLiked,setisLiked]=useState(false)
+  const [UsersId,setUsersId]=useState('')
+  const navigate=useNavigate()
+  useEffect(()=>{
+    let Token=localStorage.getItem('Token')
+    if(Token){
+      axios.get(`http://localhost:5000/User/GetUserId/${Token}`)
+      .then(result=>{
+        console.log(result.data)
+        const userId=result.data
+        setUsersId(result.data)
+        const contentId=props.cardDetail._id
+        axios.put('http://localhost:5000/Content/Likes/checkLikes',{userId,contentId})
+        .then(result=>{
+          setisLiked(true)
+        })
+        .catch(err=>{
+          console.log(err)
+        })
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    }
+  },[props])
   const AddHistory=(id)=>{
     
     let Token=localStorage.getItem('Token')
-    console.log('from history',id,Token)
     if(Token){
       axios.get(`http://localhost:5000/User/GetUserId/${Token}`)
       .then(result=>{
@@ -39,7 +62,62 @@ function DynamicCard(props) {
         console.log(err)
       })
     }
-    navigator(`/VideoPlayer/${id}`)
+    navigate(`/VideoPlayer/${id}`)
+  }
+ 
+  
+  const AddLike=(id)=>{
+    if(!isLiked){
+    axios.get(`http://localhost:5000/Content/AddLike/${id}`)
+    .then(result=>{
+      console.log(result)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    const body={
+      ContentId:id,
+      userId:UsersId
+    }
+    axios.post('http://localhost:5000/Content/Likes/AddLikes',body)
+    .then(result=>{
+      console.log(result.data)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    setisLiked(!isLiked)
+ 
+  }
+  else{
+    console.log('From Likes Else')
+    axios.get(`http://localhost:5000/Content/RemoveLike/${id}`)
+    .then(result=>{
+      console.log('Successful like removal')
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    const body={
+      ContentId:id,
+      userId:UsersId
+    }
+    axios.delete('http://localhost:5000/Content/Likes/DeleteLikes',{data:body})
+    .then(result=>{
+      console.log(result.data)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    setisLiked(!isLiked)
+   
+  }
+  }
+
+  const ShareVideo=(id)=>{
+   const url=`http://localhost:3000/VideoPlayer/${id}`
+   navigator.clipboard.writeText(url)
+   alert('URL has been Copied and ready to be shared')
   }
   // const [expanded, setExpanded] = React.useState(false);
 
@@ -48,13 +126,14 @@ function DynamicCard(props) {
   // }
   return (
     <div className='max-w-sm rounded-lg overflow-hidden shadow-md shadow-blue-300' >
-      <Card className='h-96' onClick={()=>{AddHistory(props.cardDetail._id)}}>
+      <Card className='h-96' >
        <CardMedia
         component="img"
         className='rounded-t-lg h-60'
         height="194"
         image={props.cardDetail.ImageThumbnail}
         alt="Paella dish"
+        onClick={()=>{AddHistory(props.cardDetail._id)}}
       />
        <CardHeader
         avatar={
@@ -69,12 +148,17 @@ function DynamicCard(props) {
         }
         title={props.cardDetail.Title}
         subheader={props.cardDetail.Description.slice(0,50)+'....'}
+        onClick={()=>{AddHistory(props.cardDetail._id)}}
       />
        <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+        {isLiked?<IconButton aria-label="add to favorites" className='bg-red-700' onClick={()=>{AddLike(props.cardDetail._id)}}>
+          <BsHeartFill color='red' />
+        </IconButton>:
+        <IconButton aria-label="add to favorites" onClick={()=>{AddLike(props.cardDetail._id)}}>
+          <BsHeartFill />
         </IconButton>
-        <IconButton aria-label="share">
+}
+        <IconButton aria-label="share" onClick={()=>{ShareVideo(props.cardDetail._id)}}>
           <ShareIcon />
         </IconButton>
         
