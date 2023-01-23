b171d5e2e97335926a636e401b5340801bbda6f6
kedar_devs <kedard249.kd@gmail.com>
2023-01-23 13:41:05 +0530

    Test:Logs

--- DB/DbConnection.js
+++ DB/DbConnection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 const url = process.env.DB_ConnectionString
+
 exports.ConnectDB=()=>{
 try {
+    console.log(url)
     mongoose.connect(url, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
